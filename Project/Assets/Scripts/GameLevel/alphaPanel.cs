b13d9c509b45a7ddb05d327381894aaca9f45723
Eren Yarar <eren1yarar2@gmail.com>
2021-08-11 21:48:46 +0300

    DivisionForKids Full Game v_1

---
+++ Project/Assets/Scripts/GameLevel/alphaPanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class alphaPanel : MonoBehaviour
+{
+    public GameObject alpha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        alpha.GetComponent<CanvasGroup>().DOFade(0, 2f);
+    }
+
+    
+}
---
+++ Project/Assets/Scripts/MenuLevel/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject startBtn, exitBtn;
+
+    void Start()
+    {
+        FadeOut();
+    }
+
+    // Update is called once per frame
+    void FadeOut()
+    {
+        startBtn.GetComponent<CanvasGroup>().DOFade(1, 0.8f);
+        exitBtn.GetComponent<CanvasGroup>().DOFade(1, 0.8f).SetDelay(0.5f);
+    }
+
+    public void ExitGame(){
+        Application.Quit();
+        Debug.Log("Cıkıs Yapıldı");
+    }
+
+    public void StartGameLevel(){
+        SceneManager.LoadScene("GameLevel");
+    }
+}
